d08a6a5a1d7f40c30fed542e233dc770811a6185
gnenov89 <georgi.nenov89@gmail.com>
2020-01-17 12:12:57 -0500

    Updates

---
+++ precache-manifest.cacc56e05c62f885b260ee96ca16ec68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78464b95bbc2e040ca342e2f4056120",
+    "url": "/HealthConnector/index.html"
+  },
+  {
+    "revision": "ead5ee18f7254d4ee3bb",
+    "url": "/HealthConnector/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "cb22a3c1984ba23d40ca",
+    "url": "/HealthConnector/static/js/2.d0e5ff81.chunk.js"
+  },
+  {
+    "revision": "e76927ded4e98c8bb3c1b6b2d3f786e8",
+    "url": "/HealthConnector/static/js/2.d0e5ff81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ead5ee18f7254d4ee3bb",
+    "url": "/HealthConnector/static/js/main.c11a8c16.chunk.js"
+  },
+  {
+    "revision": "85b30ff1ec356c427927",
+    "url": "/HealthConnector/static/js/runtime-main.278199df.js"
+  }
+]);
