34c8fb25783e5b8b3674c67cdd0b12a0cacb13c1
gnenov89 <georgi.nenov89@gmail.com>
2020-01-17 12:40:28 -0500

    Add SPA for GitHub Pages boilerplate

---
+++ course-directory/build/precache-manifest.5ad305393a58063e89cc96fe2e00972b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b991ffe29eaa6c5991c7ecb3a74a1c44",
+    "url": "/HealthConnector/index.html"
+  },
+  {
+    "revision": "a6b062a53e803e21e03b",
+    "url": "/HealthConnector/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "88b60340ac6cc784f434",
+    "url": "/HealthConnector/static/js/2.6a9db53b.chunk.js"
+  },
+  {
+    "revision": "e76927ded4e98c8bb3c1b6b2d3f786e8",
+    "url": "/HealthConnector/static/js/2.6a9db53b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6b062a53e803e21e03b",
+    "url": "/HealthConnector/static/js/main.8c166ac7.chunk.js"
+  },
+  {
+    "revision": "85b30ff1ec356c427927",
+    "url": "/HealthConnector/static/js/runtime-main.278199df.js"
+  }
+]);
